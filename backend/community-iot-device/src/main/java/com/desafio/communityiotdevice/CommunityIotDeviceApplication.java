b678f4f1b25378bcead13231a4fa487cfb5ee5f2
paulo <paulo.t.scariot@gmail.com>
2024-05-20 16:33:00 -0300

    build: spring boot initialized

---
+++ backend/community-iot-device/src/main/java/com/desafio/communityiotdevice/CommunityIotDeviceApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.communityiotdevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityIotDeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityIotDeviceApplication.class, args);
+	}
+
+}
---
+++ backend/community-iot-device/src/test/java/com/desafio/communityiotdevice/CommunityIotDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.communityiotdevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityIotDeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
