290b2678d061d01ea757af91cd76348c5e99b830
paulo <paulo.t.scariot@gmail.com>
2024-05-23 15:10:35 -0300

    test telnet server

---
+++ telnet-server/telnetServer.js
@@ -0,0 +1,37 @@
+const net = require('net');
+
+const PORT = 3000;
+
+const server = net.createServer((socket) => {
+    console.log('Client connected');
+
+    socket.on('data', (data) => {
+        const command = data.toString().trim();
+        console.log('Received command:', command);
+        
+        const response = processCommand(command);
+
+        socket.write(response + '\n');
+    });
+
+    socket.on('end', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+function processCommand(command) {
+    const randomNumber = Math.floor(Math.random() * 101);
+    return randomNumber;
+    // switch(command) {
+    //     case 'hello':
+    //         return 'Hello, world!';
+    //     case 'date':
+    //         return new Date().toString();
+    //     default:
+    //         return `Unknown command: ${command}`;
+    // }
+}
